e9f115dd025844b325abd22b2eec1e4b49a0cdc0
Olakunle Kuye <olakuye@gmail.com>
2024-07-18 23:34:08 +0100

    Set up basic service and controller.

---
+++ src/main/java/com/example/AddressBookApp/service/AddressBookService.java
@@ -0,0 +1,10 @@
+package com.example.AddressBookApp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressBookService {
+    private String filePath = "AddressBook"; // Can be configured via application.properties
+
+    // Methods to be implemented
+}
