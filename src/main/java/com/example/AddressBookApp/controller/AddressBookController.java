09310d72def43acba303934d421b04ac34ccf70a
Olakunle Kuye <olakuye@gmail.com>
2024-07-18 23:37:46 +0100

    Add countMales endpoint in AddressBookController.

--- src/main/java/com/example/AddressBookApp/controller/AddressBookController.java
+++ src/main/java/com/example/AddressBookApp/controller/AddressBookController.java
@@ -2,6 +2,7 @@ package com.example.AddressBookApp.controller;
 
 import com.example.AddressBookApp.service.AddressBookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,9 @@ public class AddressBookController {
     @Autowired
     private AddressBookService service;
 
-    // API endpoints to be added
+    @GetMapping("/countMales")
+    public long countMales() {
+        return service.countMales();
+    }
 }
+
